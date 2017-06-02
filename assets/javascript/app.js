94f20e97eb2f2f9adcca98911f6cb6d3c26e6686
dhobbs81 <dhobbs81@gmail.com>
2017-06-02 08:58:47 -0400

    Updated application logic to use a web api to fetch questions and answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,117 @@
 //
-// The CrystalsCollector Game
+// The Trivia Game
 //
-// The following Javascript implements the CrystalsCollector Game as 
-// described in README.md. The game asks the player to accumulate 
-// crystals to reach a total crystal value. The player wins if their total 
-// score matches the random number generated at the beginning of the game. 
+// The following Javascript implements the Basic Quiz Trivia Game as 
+// described in README.md. The game asks the player to answer series of 
+// trivia style questions. The player has a set period of time to answer 
+// as many questions as possible. After the game clock expires, a score 
+// board is shown giving the player game statistics.
+//
+// The player will have a limited amount of time to finish the quiz. 
+//
+// The game ends when the time runs out. The page will reveal the number 
+// of questions that players answer correctly and incorrectly.
+//
+// Don't let the player pick more than one answer per question.
+//
+// Don't forget to include a countdown timer.
 //
 
 // Execute in strict mode as defined by the ECMAScript version 5 standard
 "use strict";
 
 //
-// Returns a random integer between min (inclusive) and max (inclusive)
-// Using Math.round() will give you a non-uniform distribution!
+// Mashape APIs require an access key
 //
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+function getMashapeApiKey() {
+	return "6cnGhUWtqsmsh27aeRGcQTyea3Yip1hVMHAjsnMMBkGXelIRQZ";
+} 
+
+//
+// This AJAX query accesses a Mashape music trivia API
+// The music trivia provides a set of trivia questions and 
+// corresponding answers
+//
+function getTriviaQuestions(triviaQuestions) {
+	$.ajax({
+		url: 'https://roomtek-music-trivia-v1.p.mashape.com/getgamelevel?output=json', 
+		type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+		data: {}, // Additional parameters here
+		dataType: 'json',
+		complete: function (jqXHR, textStatus) {
+			// Get a reference to the array of trivia questions
+			let questions = (JSON.parse(jqXHR.responseText)).question;
+
+			// For each question in the array, add an element to the main container
+			for (var i = 0; i < questions.length; i++) {
+				let musicTrivia = Object.assign({}, questions[i]);
+				//console.log(musicTrivia);
+				addQuestion(musicTrivia, triviaQuestions);
+			}
+		},
+		// The Masahape requires the API key to be sent as part of the HTTP  
+		// request header
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("X-Mashape-Authorization", getMashapeApiKey()); 
+		}
+	});
 }
 
+//
+// Add a music trivia question to the set of trivia questions division 
+//
+function addQuestion(musicTrivia, triviaQuestions) {
+	// Append the text of the question to the trivia questions division
+	triviaQuestions.append(
+		$("<h1>").append(musicTrivia.text)
+	);
+
+	//
+	// Create a form to contain the player's answers to this question
+	//
+	let question = $("<form class='musicTrivia'>");
+
+	//
+	// For each answer to the questions, append a radio input to the
+	// question form
+	//
+	for (var i = 0; i < musicTrivia.answers.a.length; i++) {
+
+		let radioInput = {};
+
+		//
+		// Create a radio input
+		// The correct radio input is visible in the HTML
+		//
+		if (musicTrivia.answers.a[i].correct == 1) {
+			radioInput = $("<input type=\"radio\" value=\"true\">");
+		}
+		else {
+			radioInput = $("<input type=\"radio\" value=\"false\">");
+		}
+
+		var answer = Object.assign({}, musicTrivia.answers.a[i]);
+
+		// When an input changes in the form, update the form 
+		// with a trivia answer object
+		radioInput.change(function() {
+			// Uncheck the other answers
+			question.children().prop("checked", false);
+			radioInput.prop("checked", true);
+			// Assign the currently selected answer to the input form
+			// console.log(answer);	
+			question.data("answer", answer);
+		});
+
+		// Get the answer text and append it to the questions form
+		let answerText = musicTrivia.answers.a[i].value;
+		question.append(radioInput).append(answerText);
+		triviaQuestions.append(question);
+	}
+
+	// Add some space between this question and the next
+	triviaQuestions.append("<br><br>");
+}
 
 //
 // Returns a jQuery object representing the main containing element
@@ -27,85 +121,71 @@ function getMain() {
 }
 
 //
-// The game state is stored by a crystalsCollector object Game state includes 
-// wins, losses, and the set of guesses input by the user
+//  Converts a time inseconds to minutes and seconds 
 //
-var crystalsCollector = { 
-	//
-	// Game state properties
-	//
-	wins: 			0,
-	losses: 		0, 
-	totalScore: 		0,
-	currentScore: 	0,
-	crystals: 		{ red: 0, blue: 0, yellow: 0, green: 0, },
+function convertSeconds(t) {
 
-	// 
-	// The reset function returns this object to an initial state
-	//
-	reset: function() {
-		// Generate a random totalScore in the range from 19 to 120 inclusive
-		this.totalScore = getRandomInt(19, 120);
-		// Generate random crystal value in the range from 1 to 12 inclusive
-		this.crystals.red = getRandomInt(1, 12);
-		this.crystals.blue = getRandomInt(1, 12);
-		this.crystals.yellow = getRandomInt(1, 12);
-		this.crystals.green = getRandomInt(1, 12);
-		this.currentScore = 0;
-		this.display();
+    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
+
+//
+// The timer object can counts down in seconds
+//
+var timer = {
+	// Reset the timer display and countdown
+	reset: function(countdown) {
+		timer.seconds = countdown;
+		timer.running = false;
+		timer.display = convertSeconds(timer.seconds);
 	},
-
-	// 
-	// Log pertinent state information to the console
-	//
-	log: function() {
-		console.log("Total Score: " + this.totalScore);
-		console.log("Current Score: " + this.currentScore);
-		console.log(this.crystals);
+	// Start the timer interval and set the callbacks
+	start: function(countCallback, expiredCallback) {
+		if (!timer.running) {
+			timer.interval = setInterval(timer.count, 1000);
+			timer.running = true;
+			timer.countCallback = countCallback;
+			timer.expiredCallback = expiredCallback;
+		}
 	},
-
-	//
-	// Output game state to the display
-	//
-	display: function() {
-		$("#totalScore").text(this.totalScore);
-		$("#currentScore").text(this.currentScore);
-		$("#wins").text(this.wins);
-		$("#losses").text(this.losses);
-		this.log();
+	// Clears the timer interval
+	stop: function() {
+		if (timer.running) {
+			clearInterval(timer.interval);
+			timer.running = false;
+		}
 	},
-
-	//
-	// Change the game state based on color of crystal selected
-	//
-	add: function(color) {
-		switch (color) {
-			case "red":
-				this.currentScore += this.crystals.red;
-				break;
-			case "blue":
-				this.currentScore += this.crystals.blue;
-				break;
-			case "yellow":
-				this.currentScore += this.crystals.yellow;
-				break;
-			case "green":
-				this.currentScore += this.crystals.green;
-				break;
-			default:
-				break;
+	// Count down the remaining time and call count callback
+	// When the timer expires, stop the time and call the expired callback
+	count: function() {
+		if (timer.running == false) {
+			return;
 		}
 
-		if (this.currentScore == this.totalScore) {
-			this.wins++;
-			this.reset();
+		timer.seconds--;
+		timer.display = convertSeconds(timer.seconds);
+		timer.countCallback();
+
+		if (timer.seconds == 0) {
+			timer.stop();
+			timer.expiredCallback();	
 		}
-		else if (this.currentScore > this.totalScore) {
-			this.losses++;
-			this.reset();
-		}
-		this.display();
-	}
+	},
 };
 
 // MAIN PROCESS
@@ -128,183 +208,133 @@ $(function() {
 		"margin" : "0",
 		"background-image" : "url(assets/images/tiledbackground.jpg)", 
 		"background-position" : "center top", 
+		"background-size" : "100%"
 	});
 
 	//
 	// Create a heading and add to the main container
 	//
-	var heading = $("<h1>").text("Crystals Collector!").css ({
-		"float" : "left",
-		"margin" : "10px",
+	var heading = $("<h1>").text("Totally Trivia Trivia!").css ({
+		"float" : "none",
+		"width" : "80%",
+		"margin" : "0 auto",
 		"padding" : "20px",
-		"background-color" : "orange",
-		"color" : "white"
+		"background-color" : "LightCyan",
+		"color" : "blue",
+		"font-family" : '"Comic Sans MS", cursive, sans-serif',
+		"font-size" : "300%",
 	});
-
 	getMain().prepend(heading);
 
 	//
-	// Create an instructions paragraph and add to the main container
+	// Create a timer display and add to the main container
 	//
-	var instructions = $("<div>")
-	.css ({
-		"clear" : "both",
-		"float" : "left",
-		"width" : "50%",
-		"margin" : "10px",
+	var timerDisplay = $('<h1>').text("Time Remaining: ").css ({
+		"float" : "none",
+		"width" : "80%",
+		"margin" : "0 auto",
+		"margin-top" : "10px",
 		"padding" : "20px",
-		"background-color" : "pink",
-		"color" : "green"
+		"background-color" : "LightCyan",
+		"font-family" : 'Courier',
+		"color" : "blue",
 	})
 	.append(
-		$("<p>").text (
-			"You will be given a 	random number at the start of the game."
-		)
-	)
-	.append(
-		$("<p>").text (
-			"There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score."
-		)
-	)
-	.append(
-		$("<p>").text (
-			"You win the game by matching your total score to random number, you lose the game if your total score goes above the random number."
-		)
-	)
-	.append(
-		$("<p>").text (
-			"The value of each crystal is hidden from you until you click on it."
-		)
-	)
-	.append(
-		$("<p>").text (
-			"Each time when the game starts, the game will change the values of each crystal."
-		)
+		$('<div id = "timerDisplay">').text("00:00").css( { "display" : "inline" })
 	);
-	getMain().append(instructions);
+	getMain().append(timerDisplay);
 
 	//
-	// Create a score board to display the total score
+	// Create a scoreboard to display game statistics
 	//
-	var totalScore = $('<h1 id = "totalScore">')
-	.text ("0")
+	var scoreBoard = $('<div>')
 	.css ({
-		"clear" : "both",
-		"float" : "left",
-		"width" : "30%",
-		"margin" : "10px",
+		"float" : "none",
+		"width" : "80%",
+		"margin" : "0 auto",
+		"margin-top" : "10px",
+		"margin-bottom" : "10px",
 		"padding" : "20px",
-		"background-color" : "green",
+		"background-color" : "LightCyan",
 		"color" : "black"
-	});
-	getMain().append(totalScore);
-
-	//
-	// Create a score board to display the wins and losses 
-	//
-	var winsLosses = $('<div>')
-	.css ({
-		"float" : "left",
-		"width" : "20%",
-		"margin" : "10px",
-		"padding" : "0px",
-		"background-color" : "aqua",
-		"color" : "black",
 	})
 	.append(
-		$('<p>').text("Wins: ").append(
-			$('<div id = "wins">').text("0").css( { "display" : "inline" })
+		$('<h2>').text("All Done!")
+	).append(
+		$('<h3>').text("Correct Answers: ").append(
+			$('<div id = "correct">').text("0").css( { "display" : "inline" })
 		)
 	).append(
-		$('<p>').text("Losses: ").append(
-			$('<div id = "losses">').text("0").css( { "display" : "inline" })
+		$('<h3>').text("Incorrect Answers: ").append(
+			$('<div id = "incorrect">').text("0").css( { "display" : "inline" })
+		)
+	).append(
+		$('<h3>').text("Unanswered: ").append(
+			$('<div id = "unanswered">').text("0").css( { "display" : "inline" })
 		)
 	);
-	getMain().append(winsLosses);
+	getMain().append(scoreBoard);
+	scoreBoard.hide();
 
 	//
-	// Create a series of image to represent the crystals
-	// When clicked, update the game state
+	// The trivia questions division contains questions and the users answers
 	//
-	var redCrystal = $('<img src = "assets/images/red.png">')
+    	var triviaQuestions = $("<div class='triviaQuestions'>")
 	.css ({
-		"clear" : "both",
-		"float" : "left",
-		"margin" : "0px",
-		"padding" : "0px",
-		"margin-left" : "20px",
-		"width" : "100px",
-		"height" : "100px",
-	});
-	getMain().append(redCrystal);
-	redCrystal.on('click', function() { crystalsCollector.add("red"); });
-
-	var blueCrystal = $('<img src = "assets/images/blue.png">')
-	.css ({
-		"float" : "left",
-		"margin" : "0px",
-		"padding" : "0px",
-		"width" : "100px",
-		"height" : "100px",
-	});
-	getMain().append(blueCrystal);
-	blueCrystal.on('click', function() { crystalsCollector.add("blue"); });
-
-	var yellowCrystal = $('<img src = "assets/images/yellow.png">')
-	.css ({
-		"float" : "left",
-		"margin" : "0px",
-		"width" : "100px",
-		"height" : "100px",
-		"padding" : "0px",
-	});
-	getMain().append(yellowCrystal);
-	yellowCrystal.on('click', function() { crystalsCollector.add("yellow"); });
-
-	var greenCrystal = $('<img src = "assets/images/green.png">')
-	.css ({
-		"float" : "left",
-		"margin" : "0px",
-		"margin-left" : "20px",
-		"padding" : "0px",
-		"width" : "100px",
-		"height" : "100px",
-	});
-	getMain().append(greenCrystal);
-	greenCrystal.on('click', function() { crystalsCollector.add("green"); });
-
-	//
-	// Create a section to display the current score text
-	//
-	var currentScoreText = $('<p>')
-	.text ("Your total score is: ")
-	.css ({
-		"clear" : "both",
-		"float" : "left",
-		"width" : "30%",
-		"margin" : "10px",
+		"float" : "none",
+		"width" : "80%",
+		"margin" : "0 auto",
+		"margin-top" : "10px",
 		"padding" : "20px",
-		"background-color" : "LightSlateGray",
-		"color" : "white"
+		"background-color" : "LightCyan",
+		"color" : "black"
 	});
-	getMain().append(currentScoreText);
+	getMain().append(triviaQuestions);
 
-	//
-	// Create a score board to display the current score
-	//
-	var currentScore = $('<h1 id="currentScore">')
-	.text ("0")
-	.css ({
-		"clear" : "both",
-		"float" : "left",
-		"width" : "30%",
-		"margin" : "10px",
-		"padding" : "20px",
-		"background-color" : "LightSlateGray",
-		"color" : "white"
-	});
-	getMain().append(currentScore);
+	// Get a set of trivia questions from the trivia questions API
+	getTriviaQuestions(triviaQuestions);
 
-	// Initialize a Game object
-	crystalsCollector.reset();
+    	// Initialize and start the game timer
+    	timer.reset(5);
+    	timer.start(
+    		// When a timer event occurs display the remaining time
+    		function() {
+    			$("#timerDisplay").text(timer.display);
+    		},
+    		// When the timer expires, show the scoreboard
+    		// Hide the trivia questions and tally the player's
+    		// score
+	    	function() {
+	    		triviaQuestions.hide();
+	    		scoreBoard.show();
+			getMain().css({
+				"width" : "100%",
+				"height" : "100%",
+			});
+
+			var correct = 0;
+			var incorrect = 0;
+			var unanswered = 0;
+
+			// Get the player's answers and tally his score
+			$(".musicTrivia").each(function() {
+				let answer = $(this).data("answer");
+
+				if (answer === undefined) {
+					unanswered++;
+				}
+				else if (answer.correct == 1) {
+					correct++;
+				}
+				else {
+					incorrect++;
+				}
+			});
+
+			// Display the tally
+			$("#correct").text(correct);
+			$("#incorrect").text(incorrect);
+			$("#unanswered").text(unanswered);
+	    	}
+    	);
  });
